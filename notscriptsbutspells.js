097c8af7c672f6e73393046b7eb822f69a6fb75a
Sander <sander.schaass@gmail.com>
2026-01-15 14:23:01 +0100

    tomato soup is soup made of tomatoes

---
+++ notscriptsbutspells.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Orange Juice")
+});
+
+const hamMenu = document.querySelector('.ham-menu');
+const offScreenMenu = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active');
+})
